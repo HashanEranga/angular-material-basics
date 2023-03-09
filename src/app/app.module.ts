082d5f2d7ff3855884831bfc7216103a2532dfca
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-03-09 16:29:17 +0530

    created initial material ui setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material/material.module";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterOutlet,
+    RouterLink,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
